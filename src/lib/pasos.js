a1c5f0c41ffb0595585644052f1f0f6f77b259a6
Wilmer Cuevas <wilmerjesus442002@gmail.com>
2025-09-25 04:01:07 +0300

    Texto de los pasos editados

--- src/lib/pasos.js
+++ src/lib/pasos.js
@@ -1,24 +1,24 @@
 const pasos = [
 
     {
-        titulo: "Consulta Inicial",
-        descripcion: "Comenzamos con una consulta para discutir tus ideas, estilo y ubicación del tatuaje. Esto nos ayuda a entender tu visión y expectativas."
+        titulo: "Cotización",
+        descripcion: "Deben enviar imágenes referenciales de la idea que se desea tatuar especificando el estilo, tamaño y la zona a tatuar. Esto nos ayudará a entender tu visión y expectativas."
     },
     {
         titulo: "Diseño Personalizado",
-        descripcion: "Basándonos en nuestra conversación, creamos un diseño personalizado que refleje tu estilo y preferencias. Te enviaremos bocetos para tu revisión y ajustes."
+        descripcion: "Basándonos en nuestra conversación, creamos un diseño personalizado que refleje tu estilo y preferencias. Te enviaremos bocetos para tu revisión y ajustes. Para este paso es importante que el cliente realice un abono para tener garantía del trabajo."
     },
     {
-        titulo: "Preparación para el Tatuaje",
-        descripcion: "Antes de la sesión, te proporcionaremos instrucciones sobre cómo preparar tu piel y qué esperar durante el proceso de tatuaje."
+        titulo: "Agenda y Disponibilidad",
+        descripcion: "Al tener el diseño culminado y el abono cancelado, el cliente debe consultar con mínimo dos días de anticipación la disponibilidad de días para seleccionar el que más le convenga."
     },
     {
         titulo: "Sesión de Tatuaje",
-        descripcion: "Durante la sesión, nos aseguramos de que estés cómodo y bien informado sobre cada paso. Utilizamos técnicas higiénicas y seguras para garantizar la mejor experiencia."
+        descripcion: "Durante la sesión, nos aseguramos de que estés cómodo y bien informado sobre cada paso. Utilizamos técnicas respetando la bioseguridad para garantizar un buen resultado libre de la contaminación cruzada."
     },
     {
         titulo: "Cuidado Posterior",
-        descripcion: "Después de completar el tatuaje, te daremos instrucciones detalladas para el cuidado posterior, asegurando una curación adecuada y la mejor apariencia del tatuaje a largo plazo."
+        descripcion: "Después de completar el tatuaje, te daremos instrucciones detalladas para el cuidado posterior, asegurando una curación adecuada y la mejor apariencia del tatuaje a largo plazo. Cualquier duda durante el proceso de cicatrización no dudes en contactarnos."
     },
 
 ];
