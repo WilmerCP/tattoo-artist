2d2a1c626e3105dbbb33e6d46e0e618a0d759056
Wilmer Cuevas <wilmerjesus442002@gmail.com>
2025-10-08 04:23:20 +0300

    Fixed arrow icon sizing

--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -1,18 +1,20 @@
 import { IoMdArrowDropdown } from "react-icons/io";
-import { IoMdArrowDropup } from "react-icons/io";
 import { useState } from "react";
 
 export default function Pregunta({ pregunta, respuesta }) {
 
     const [isOpen, setIsOpen] = useState(false);
 
-    const icon = isOpen ? <IoMdArrowDropup className="text-5xl"/> : <IoMdArrowDropdown className="text-5xl"/>;
 
     return (
         <div className="border-b border-gray-700 py-4">
             <div className="cursor-pointer text-xl mb-2 flex flex-nowrap gap-2 items-end justify-between" onClick={() => setIsOpen(!isOpen)}>
                 <h3 className="md:text-2xl text-xl font-semibold mb-2">{pregunta}</h3>
-                {icon}
+                <span className={`inline-block w-[40px] h-[40px] transition-transform duration-300 ${isOpen ? 'transform rotate-180' : ''}`}>
+                    <IoMdArrowDropdown
+                        style={{ fontSize: '40px' }}
+                    />
+                </span>
             </div>
 
             {isOpen && <p className="md:text-lg text-base">{respuesta}</p>}
